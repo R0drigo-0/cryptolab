acf91314c68dfed4797dc23051c333853ca03030
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2024-11-24 21:56:34 +0100

    Grid

--- src/views/Components/SidebarView.js
+++ src/views/Components/SidebarView.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
 import LinkIcon from "@mui/icons-material/Link";
 import LockIcon from "@mui/icons-material/Lock";
 import InputIcon from "@mui/icons-material/Input";
@@ -11,30 +12,27 @@ import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import FingerprintIcon from "@mui/icons-material/Fingerprint";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-
-import { Sidebar, Menu, MenuItem } from "react-pro-sidebar";
-
 import SidebarController from "../../controllers/SidebarController";
+import "../../styles/SidebarView.css";
 
 const SidebarView = () => {
-  const [collapsed, setCollapsed] = useState(SidebarController.getCollapsed());
+  const [collapsed, setCollapsed] = useState(false);
 
   const handleToggle = () => {
-    SidebarController.toggleSidebar();
-    setCollapsed(SidebarController.getCollapsed());
+    setCollapsed(!collapsed);
   };
 
   const handleSelectItem = (item) => {
     SidebarController.setSelectItem(item);
-  }
+  };
 
   return (
-    <div className={`sidebar ${collapsed ? 'collapsed' : 'expanded'}`}>
+    <div className={`sidebar-container ${collapsed ? 'collapsed' : 'expanded'}`}>
       <button className="toggle-button" onClick={handleToggle}>
         {collapsed ? <ChevronRightIcon /> : <ChevronLeftIcon />}
       </button>
-      <Sidebar collapsed={collapsed} width={"200px"}>
-        <Menu>
+      <Sidebar className="sidebar" collapsed={collapsed} width={"200px"} height="100vh">
+        <Menu height="100vh">
           <MenuItem icon={<InputIcon />} onClick={() => handleSelectItem('Input')}>Input</MenuItem>
           <MenuItem icon={<OutputIcon />} onClick={() => handleSelectItem('Output')}>Output</MenuItem>
           <MenuItem icon={<LockIcon />} onClick={() => handleSelectItem('Encrypt')}>Encrypt</MenuItem>
@@ -45,8 +43,6 @@ const SidebarView = () => {
           <MenuItem icon={<SecurityIcon />} onClick={() => handleSelectItem('Private Key')}>Private Key</MenuItem>
           <MenuItem icon={<SwapHorizIcon />} onClick={() => handleSelectItem('Xor')}>Xor</MenuItem>
           <MenuItem icon={<LinkIcon />} onClick={() => handleSelectItem('Concatenate')}>Concatenate</MenuItem>
-          <MenuItem icon={<ChevronLeftIcon />} onClick={() => handleSelectItem('Shift Left')}>Shift Left</MenuItem>
-          <MenuItem icon={<ChevronRightIcon />} onClick={() => handleSelectItem('Shift Right')}>Shift Right</MenuItem>
         </Menu>
       </Sidebar>
     </div>

1401d23d847112cf7b1f4990235406356f183c7b
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2024-11-24 10:03:06 +0100

    Update

--- src/views/OpenDesignView.js
+++ src/views/OpenDesignView.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React from "react";
+import "../styles/SidebarView.css";
+import "../styles/OpenDesignView.css";
+import SidebarView from "./Components/SidebarView";
 
 const OpenDesignView = () => {
-  return (<div>OpenDesignView</div>);
+  return (
+    <div>
+      <SidebarView />
+      <div className="grid-bg">
+        <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
+          <defs>
+            <pattern
+              id="smallGrid"
+              width="10"
+              height="10"
+              patternUnits="userSpaceOnUse"
+            >
+              <path
+                d="M 10 0 L 0 0 0 10"
+                fill="none"
+                stroke="gray"
+                strokeWidth="0.5"
+              />
+            </pattern>
+            <pattern
+              id="grid"
+              width="100"
+              height="100"
+              patternUnits="userSpaceOnUse"
+            >
+              <rect width="100" height="100" fill="url(#smallGrid)" />
+              <path
+                d="M 100 0 L 0 0 0 100"
+                fill="none"
+                stroke="gray"
+                strokeWidth="1"
+              />
+            </pattern>
+          </defs>
+          <rect width="100%" height="100%" fill="url(#grid)" />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default OpenDesignView;

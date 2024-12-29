ee02e2e45b0b5ac39daaa43a42970ea1934e3322
Rodrigo <101797599+R0drigo-0@users.noreply.github.com>
2024-12-29 19:22:33 +0100

    fix remove

--- src/views/OpenDesignView.js
+++ src/views/OpenDesignView.js
@@ -1,3 +1,4 @@
+import React, { useCallback, useEffect, useState } from "react";
 import {
   ReactFlow,
   Background,
@@ -7,8 +8,14 @@ import {
   addEdge,
   Controls,
   MiniMap,
+  NodeResizer,
+  Handle,
+  Position
 } from "@xyflow/react";
-
+import "@xyflow/react/dist/style.css";
+import "../styles/OpenDesignView.css"
+import SidebarView from "./SidebarView";
+import OpenDesignController from "../controllers/OpenDesignController";
 import {
   ConcatenateNode,
   CustomResizerNode,
@@ -22,24 +29,10 @@ import {
   ResizableNode,
   ResizableNodeSelected,
   SeedNode,
-  XorNode,
+  XorNode
 } from "./components/nodes";
 
-import { useState } from "react";
-
-import React from "react";
-import SidebarView from "./SidebarView";
-import OpenDesignController from "../controllers/OpenDesignController";
-
-import "@xyflow/react/dist/style.css";
-import "../styles/OpenDesignView.css";
-
 const OpenDesignView = () => {
-  const [nodes, setNodes] = useNodesState([]);
-  const [edges, setEdges] = useEdgesState([]);
-  const [selectedNodes, setSelectedNodes] = useState([]);
-
-  console.log(nodes);
   const nodeTypes = {
     ResizableNode,
     ResizableNodeSelected,
@@ -54,6 +47,23 @@ const OpenDesignView = () => {
     PrivateKeyNode,
     XorNode,
     ConcatenateNode,
+  }
+  
+  const snapGrid = [20,20];
+  const [selectedNodes, setSelectedNodes] = useState([]);
+  const [nodes, setNodes, onNodesChange] = useNodesState([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+  const handleNewEdge = (params) => {
+    const newEdge = {
+      id: `${edges.length + 1}`,
+      source: params.source,
+      target: params.target,
+      animated: true,
+      type: "smoothstep"
+    };
+    OpenDesignController.addEdge(params.source, params.target);
+    setEdges((eds) => addEdge(newEdge, eds));
   };
 
   const handleNewNode = (item) => {
@@ -67,29 +77,12 @@ const OpenDesignView = () => {
     setNodes((nds) => [...nds, newNode]);
   };
 
-  const handleNewEdge = (params) => {
-    const newEdge = {
-      id: `${edges.length + 1}`,
-      source: params.source,
-      target: params.target,
-      animated: true,
-      type: "smoothstep",
-    };
-    OpenDesignController.addEdge(params.source, params.target);
-    setEdges((eds) => addEdge(newEdge, eds));
-  };
-
   const handleNodesDelete = (deletedNodes) => {
     deletedNodes = deletedNodes["nodes"];
-    deletedNodes.forEach((node) => {
+    deletedNodes.forEach(node => {
       OpenDesignController.removeNode(node.id);
     });
-    setNodes((nds) =>
-      nds.filter(
-        (node) =>
-          !deletedNodes.some((deletedNode) => deletedNode.id === node.id)
-      )
-    );
+    setNodes((nds) => nds.filter(node => !deletedNodes.some(deletedNode => deletedNode.id === node.id)));
   };
 
   const onSelectionChange = ({ nodes, edges }) => {
@@ -98,35 +91,35 @@ const OpenDesignView = () => {
 
   return (
     <div>
-      <SidebarView
-        onNewNode={handleNewNode}
-        selectedNodes={selectedNodes}
-        onNodesChange={setNodes}
-      />
+      <SidebarView onNewNode={handleNewNode} selectedNodes={selectedNodes} onNodesChange={setNodes} />
       <div className="grid-bg">
         <ReactFlow
           fitView
-          edges={edges}
+          snapToGrid
           nodes={nodes}
+          edges={edges}
+          snapGrid={snapGrid}
           nodeTypes={nodeTypes}
           onConnect={handleNewEdge}
           onDelete={handleNodesDelete}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
           onSelectionChange={onSelectionChange}
         >
           <Background color="#ccc" variant={BackgroundVariant.Lines} />
-          <MiniMap pannable zoomable position="bottom-right" />
-          <MiniMap
-            nodeStrokeColor={(n) => {
-              if (n.type === "InputNode") return "#0041d0";
-              if (n.type === "OutputNode") return "#ff0072";
-              if (n.type === "EncryptNode") return "#ff0072";
-            }}
-            nodeColor={(n) => {
-              if (n.type === "selectorNode") return "#ff0072";
-              return "#fff";
-            }}
-          />
-          <Controls className="horizontal-controls" position="bottom-right" />
+            <MiniMap  pannable zoomable position="bottom-right" />
+            <MiniMap
+              nodeStrokeColor={(n) => {
+                if (n.type === 'InputNode') return '#0041d0';
+                if (n.type === 'OutputNode') return  '#ff0072';
+                if (n.type === 'EncryptNode') return '#ff0072';
+              }}
+              nodeColor={(n) => {
+                if (n.type === 'selectorNode') return '#ff0072';
+                return '#fff';
+              }}
+            />
+            <Controls className="horizontal-controls" position="bottom-right" />
         </ReactFlow>
       </div>
     </div>

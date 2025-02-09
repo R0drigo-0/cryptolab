9f72fae6647417ea2d0415a86dc836738b2ddd93
Rodrigo <noreply@cryptolab.com>
2025-02-09 17:20:45 +0100

    HomePage style v1

--- src/views/HomePageView.jsx
+++ src/views/HomePageView.jsx
@@ -9,6 +9,8 @@ import { useNavigate } from "react-router-dom";
 import { Button, Container, Row, Col, Card } from "react-bootstrap";
 import Navbar from "./components/Navbar";
 
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+
 const HomePageView = () => {
   const navigate = useNavigate();
   const [controller] = useState(new HomePageController());
@@ -37,20 +39,41 @@ const HomePageView = () => {
   }, [controller]);
 
   return (
-    <div>
-      <div
-        className={`sticky-header ${isHeaderVisible ? "visible" : "hidden"}`}
-      >
-        <Navbar />
-      </div>
+    <>
+      <Navbar />
       <div className={`d-flex flex-column justify-content-center align-items-center ${styles.homepageContainer}`}>
-        <div className={styles.animatedBackground}></div>
-        <h1 className="text-center">
-          An interactive visual
-          <br />
-          way to learn
-        </h1>
-        <img src={logo} alt="Logo" className={styles.logo} />
+        <div className={styles.gradientBg}>
+          <div className={styles.mainContainer}>
+            <h1>
+              <span className="roboto-bold">An</span>
+              <span className="interactive_visual roboto-bold">interactive visual</span>
+              <br />
+              <span className="roboto-bold">way to learn</span>
+            </h1>
+            <div className={styles.content}>
+              <div className={styles.logoContainer}>
+                <img src={logo} className={styles.logo} />
+              </div>
+              <div className={styles.scrollIndicator}>
+                <ArrowDownwardIcon />
+              </div>
+            </div>
+          </div>
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8" result="goo" />
+                <feBlend in="SourceGraphic" in2="goo" />
+              </filter>
+            </defs>
+          </svg>
+          <div className={styles.gradientsContainer}>
+            <div className={styles.g1}></div>
+            <div className={styles.g2}></div>
+            <div className={styles.g3}></div>
+          </div>
+        </div>
       </div>
       <Container className={styles.optionsContainer}>
         <Row>
@@ -71,7 +94,7 @@ const HomePageView = () => {
           </Button>
         </div>
       </Container>
-    </div>
+    </>
   );
 };
 
--- src/views/components/Navbar.jsx
+++ src/views/components/Navbar.jsx
@@ -2,15 +2,36 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import styles from "../../styles/NavbarView.module.css";
 import { useNavigate } from "react-router-dom";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import logo from "../../assets/logo.svg";
 import { Button, Container, Row, Col, Card, Nav } from "react-bootstrap";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [isVisible, setIsVisible] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const handleScroll = () => {
+    if (window.scrollY > lastScrollY) {
+      // Scrolling down
+      setIsVisible(true);
+    } else {
+      // Scrolling up
+      setIsVisible(false);
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY]);
+
   return (
     <Container
-      className={`d-flex justify-content-between align-items-center ${styles.container}`}
+      className={`min-vw-100 d-flex justify-content-between align-items-center ${styles.container} ${isVisible ? styles.visible : styles.hidden}`}
     >
       <div className="d-flex align-items-center" onClick={() => navigate("/")}>
         <img src={logo} alt="Cryptolab" className={styles.logo} />
